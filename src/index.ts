eb5e3c92dbe39d74c5832697d7da2c4084f2a275
Igor Belyanskiy <igorexa256@gmail.com>
2020-11-18 03:15:33 +0300

    rename campaign

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import { Base } from './base'
-import { Campaign } from './campaigns'
+import { Campaign } from './campaign'
 import { POS } from './pos'
 import { Partner } from './partner'
 import { applyMixins } from './utils'
